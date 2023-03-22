ffcc75c3192e9dc0f640707f0deb48d1967bab89
rakhi.kumaribce@gmail.com <rakhi.kumaribce@gmail.com>
2023-03-22 15:36:55 +0530

    episode-1 completed

---
+++ App.js
@@ -0,0 +1,16 @@
+const parent = React.createElement('div', { id: 'parent' },
+    [
+        React.createElement('div', { id: 'child1' },
+            [React.createElement('h1', { id: 'heading1' }, 'Heading 1'),
+            React.createElement('h2', { id: 'heading2' }, 'Heading 2')]
+        ),
+        React.createElement('div', { id: 'child2' },
+            [React.createElement('h1', { id: 'heading1' }, 'Heading 1'),
+            React.createElement('h2', { id: 'heading2' }, 'Heading 2')]
+        )
+    ]
+)
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(parent)
